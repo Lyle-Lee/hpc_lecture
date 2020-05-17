3b7abf017a643d7c46d828705a7025f1ae1d51fd
Lyle Lee <lyle@Lyles-MacBook-Pro.local>
2020-05-17 20:07:36 +0900

    submit homework for MPI

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -13,24 +13,35 @@ int main(int argc, char** argv) {
   int size, rank;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  Body ibody[N/size], jbody[N/size];
+  int dN = N/size;
+  Body ibody[dN], jbody[dN];
   srand48(rank);
-  for(int i=0; i<N/size; i++) {
+  for(int i=0; i<dN; i++) {
     ibody[i].x = jbody[i].x = drand48();
     ibody[i].y = jbody[i].y = drand48();
     ibody[i].m = jbody[i].m = drand48();
     ibody[i].fx = jbody[i].fx = ibody[i].fy = jbody[i].fy = 0;
   }
-  int recv_from = (rank + 1) % size;
   int send_to = (rank - 1 + size) % size;
   MPI_Datatype MPI_BODY;
   MPI_Type_contiguous(5, MPI_DOUBLE, &MPI_BODY);
   MPI_Type_commit(&MPI_BODY);
   for(int irank=0; irank<size; irank++) {
-    MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
-    MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    for(int i=0; i<N/size; i++) {
-      for(int j=0; j<N/size; j++) {
+    Body kbody[dN];
+    for(int i=0; i<dN; i++) {
+    kbody[i].x = jbody[i].x;
+    kbody[i].y = jbody[i].y;
+    kbody[i].m = jbody[i].m;
+    kbody[i].fx = jbody[i].fx;
+    kbody[i].fy = jbody[i].fy;
+    }
+    MPI_Win win;
+    MPI_Win_create(jbody, dN*sizeof(Body), sizeof(Body), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+    MPI_Win_fence(0, win);
+    MPI_Put(kbody, dN, MPI_BODY, send_to, 0, dN, MPI_BODY, win);
+    MPI_Win_fence(0, win);
+    for(int i=0; i<dN; i++) {
+      for(int j=0; j<dN; j++) {
         double rx = ibody[i].x - jbody[j].x;
         double ry = ibody[i].y - jbody[j].y;
         double r = std::sqrt(rx * rx + ry * ry);
@@ -40,12 +51,13 @@ int main(int argc, char** argv) {
         }
       }
     }
+    MPI_Win_free(&win);
   }
   for(int irank=0; irank<size; irank++) {
     MPI_Barrier(MPI_COMM_WORLD);
     if(irank==rank) {
-      for(int i=0; i<N/size; i++) {
-        printf("%d %g %g\n",i+rank*N/size,ibody[i].fx,ibody[i].fy);
+      for(int i=0; i<dN; i++) {
+        printf("%d %g %g\n",i+rank*dN,ibody[i].fx,ibody[i].fy);
       }
     }
   }
